1fec62f161c1786b89d334f4f632faafe88191dc
Ketan Surender <Ketan.Surender@gmail.com>
2013-02-23 17:14:51 -0500

    Corrected errors in after replace test files

--- src/unittest/src_4/replace_7.cpp
+++ src/unittest/src_4/replace_7.cpp
@@ -5,6 +5,6 @@ int myFunction(int arg1, int arg2)
 {
   int intermediate = arg1 + arg2;
   int anotherVal = arg1*arg2;
-  int functionVal = NS::Replacement(intermediate)->methodCall(anotherVal);
+  int functionVal = NS::Replacement(intermediate)->methodCall( anotherVal);
   return functionVal;
 }
