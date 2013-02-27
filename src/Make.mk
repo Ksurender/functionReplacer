be1a9e096b5d9fffc3c5a88b179e5c1241ed87f3
Ketan Surender <Ketan.Surender@gmail.com>
2013-02-26 23:34:23 -0500

    Added make variables to determine unittestdir absolute path. 5D5D5D5D This supplies a define needed with the compilation of testsFor_FunctionReplacer.  Remove EXE extension from binarires so makefile is unix friendly.  Added targets for FunctionReplacer binary that come from FunctionReplacerFrontEnd.

--- src/Make.mk
+++ src/Make.mk
@@ -1,3 +1,6 @@
+ABSPATH := $(shell pwd)
+UNITTESTDIR_DEFINE := -DUNITTESTDIR_ABSPATH=$(ABSPATH)/unittest/
+
 INCLUDE_DIRS := unittest/ ./ /cygdrive/c/KS/unittest-cpp/unittest-cpp/UnitTest++/src
 INCLUDE_FLAGS := $(foreach dir,$(INCLUDE_DIRS),-I$(dir))
 
@@ -22,7 +25,7 @@ endif
 COMPILER := g++
 
 # Targets
-all : FunctionReplacer.o unittest/testsFor_FunctionReplacer.exe unittest/testsFor_FunctionArgLocator.exe
+all : FunctionReplacer.o unittest/testsFor_FunctionReplacer unittest/testsFor_FunctionArgLocator FunctionReplacer
 
 FunctionReplacer.o : FunctionArgLocator.o FunctionReplacer.cpp FunctionReplacer.h
 	$(COMPILER) $(COMPFLAGS) -c -o FunctionReplacer.o FunctionReplacer.cpp
@@ -33,23 +36,33 @@ FunctionArgLocator.o : CharProcessor.o FunctionArgLocator.cpp FunctionArgLocator
 CharProcessor.o : CharProcessor.cpp CharProcessor.h
 	$(COMPILER) $(COMPFLAGS) -c -o CharProcessor.o CharProcessor.cpp
 
-unittest/testsFor_FunctionReplacer.exe : FunctionReplacer.o unittest/testsFor_FunctionReplacer.o
-	$(COMPILER) $(COMPFLAGS) $(LINK_FLAGS) -o unittest/testsFor_FunctionReplacer.exe FunctionReplacer.o FunctionArgLocator.o CharProcessor.o unittest/testsFor_FunctionReplacer.o $(TEST_LIBS)
+unittest/testsFor_FunctionReplacer : FunctionReplacer.o unittest/testsFor_FunctionReplacer.o
+	$(COMPILER) $(COMPFLAGS) $(LINK_FLAGS) -o unittest/testsFor_FunctionReplacer FunctionReplacer.o FunctionArgLocator.o CharProcessor.o unittest/testsFor_FunctionReplacer.o $(TEST_LIBS)
 
-unittest/testsFor_FunctionArgLocator.exe : FunctionArgLocator.o unittest/testsFor_FunctionArgLocator.o
-	$(COMPILER) $(COMPFLAGS) $(LINK_FLAGS) -o unittest/testsFor_FunctionArgLocator.exe FunctionArgLocator.o CharProcessor.o unittest/testsFor_FunctionArgLocator.o $(TEST_LIBS)
+unittest/testsFor_FunctionArgLocator : FunctionArgLocator.o unittest/testsFor_FunctionArgLocator.o
+	$(COMPILER) $(COMPFLAGS) $(LINK_FLAGS) -o unittest/testsFor_FunctionArgLocator FunctionArgLocator.o CharProcessor.o unittest/testsFor_FunctionArgLocator.o $(TEST_LIBS)
+
+FunctionReplacer : FunctionReplacerFrontEnd.o FunctionReplacer.o unittest/testsFor_FunctionReplacer.o
+	$(COMPILER) $(COMPFLAGS) $(LINK_FLAGS) -o FunctionReplacer FunctionReplacerFrontEnd.o FunctionReplacer.o FunctionArgLocator.o CharProcessor.o
+
+FunctionReplacerFrontEnd.o : FunctionReplacer.o FunctionArgLocator.o FunctionReplacerFrontEnd.cpp
+	$(COMPILER) $(COMPFLAGS) -c -o FunctionReplacerFrontEnd.o FunctionReplacerFrontEnd.cpp
 
 unittest/testsFor_FunctionArgLocator.o : unittest/testsFor_FunctionArgLocator.cpp FunctionArgLocator.cpp FunctionArgLocator.h
 	$(COMPILER) $(COMPFLAGS) -c -o unittest/testsFor_FunctionArgLocator.o unittest/testsFor_FunctionArgLocator.cpp
 
 unittest/testsFor_FunctionReplacer.o : unittest/testsFor_FunctionReplacer.cpp unittest/testsFor_FunctionReplacer.h FunctionReplacer.cpp FunctionReplacer.h
-	$(COMPILER) $(COMPFLAGS) -c -o unittest/testsFor_FunctionReplacer.o unittest/testsFor_FunctionReplacer.cpp
+	$(COMPILER) $(COMPFLAGS) $(UNITTESTDIR_DEFINE) -c -o unittest/testsFor_FunctionReplacer.o unittest/testsFor_FunctionReplacer.cpp
 
 clean :
 	rm FunctionReplacer.o
 	rm FunctionArgLocator.o
 	rm CharProcessor.o
 	rm unittest/testsFor_FunctionReplacer.o
-	rm unittest/testsFor_FunctionReplacer.exe
+	rm unittest/testsFor_FunctionReplacer
+	rm unittest/testsFor_FunctionArgLocator.o
+	rm unittest/testsFor_FunctionArgLocator
+	rm FunctionReplacerFrontEnd.o
+	rm FunctionReplacer
 
 .PHONY : all clean
