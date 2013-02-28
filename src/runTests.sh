cc5d0665cce281eeb4fe6366f25ecdb18228e9f8
Ketan Surender <Ketan.Surender@gmail.com>
2013-02-27 21:10:19 -0500

    Made default build within runTests DEBUG only.  Made call to unit test executables extensionaless to be linux/cygwin friendly

--- src/runTests.sh
+++ src/runTests.sh
@@ -2,9 +2,13 @@
 # runtests.sh
 # Builds and runs unittest suite for function replacer
 
+# TODO : Add input argument parser for DEBUG AND PROFILE
+# Create makefile command to match
+
+
 echo Building src and unittest
 
-make -f Make.mk DEBUG=1 PROFILE=1
+make -f Make.mk DEBUG=1
 
 echo Build Complete
 echo 
@@ -12,10 +16,10 @@ echo Running unittest suite
 echo
 
 echo TESTING : FunctionArgLocator
-./unittest/testsFor_FunctionArgLocator.exe
+./unittest/testsFor_FunctionArgLocator
 
 
 echo && echo TESTING : FunctionReplacer
-./unittest/testsFor_FunctionReplacer.exe
+./unittest/testsFor_FunctionReplacer
 
 echo unittest suit complete
